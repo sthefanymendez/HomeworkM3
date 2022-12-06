6b6568b24eb2fca216cfb19cf378fd60f4380566
FelipeCiroM <felipe.cirom@udea.edu.co>
2022-12-06 11:42:59 -0500

    clean integration 01-node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,22 +1,7 @@
 const process = require("process");
 const commands = require("./commands/index.js");
 
-function print(output) {
-  process.stdout.write(output);
-  process.stdout.write("\nprompt > ");
-}
-
 function bash() {
-  process.stdout.write("prompt > ");
-  process.stdin.on("data", function (data) {
-    const args = data.toString().trim();
-    const cmd = args.split(" ")[0];
-    if (commands[cmd]) {
-      commands[cmd](print, args);
-    } else {
-      print("command not found: " + cmd);
-    }
-  });
 }
 
 bash();
