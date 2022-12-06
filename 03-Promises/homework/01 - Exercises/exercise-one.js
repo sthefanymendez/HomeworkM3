05e789a06806c9f0b6bf94e2b813614c7053476f
MPrecelle <tinchoprecelle@gmail.com>
2022-12-06 11:13:26 -0300

    Continue testing exercise-two

--- 03-Promises/homework/01 - Exercises/exercise-one.js
+++ 03-Promises/homework/01 - Exercises/exercise-one.js
@@ -27,135 +27,135 @@ args.forEach(function (arg) {
 });
 
 function problemA() {
-  // callback version 
-  // exerciseUtils.readFile("poem-one/stanza-01.txt", function (err, stanza) {
-  //   exerciseUtils.blue(stanza);
-  // });
+  // callback version
+  exerciseUtils.readFile("poem-one/stanza-01.txt", function (err, stanza) {
+    exerciseUtils.blue(stanza);
+  });
 
   // promise version
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-01.txt")
-    .then((stanza) => exerciseUtils.blue(stanza));
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-01.txt")
+  //   .then((stanza) => exerciseUtils.blue(stanza));
 }
 
 function problemB() {
   // callback version
-  // exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
-  //   exerciseUtils.blue(stanza2);
-  // });
-  // exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-  //   exerciseUtils.blue(stanza3);
-  // });
+  exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
+    exerciseUtils.blue(stanza2);
+  });
+  exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+    exerciseUtils.blue(stanza3);
+  });
 
   // promise version
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-02.txt")
-    .then((stanza) => exerciseUtils.blue(stanza));
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-03.txt")
-    .then((stanza) => exerciseUtils.blue(stanza));
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-02.txt")
+  //   .then((stanza) => exerciseUtils.blue(stanza));
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-03.txt")
+  //   .then((stanza) => exerciseUtils.blue(stanza));
 }
 
 function problemC() {
   // callback version
-  // exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
-  //   exerciseUtils.blue(stanza2);
-  //   exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-  //     exerciseUtils.blue(stanza3);
-  //   });
-  // });
+  exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
+    exerciseUtils.blue(stanza2);
+    exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+      exerciseUtils.blue(stanza3);
+    });
+  });
 
   // promise version (hint: don't need to nest `then` calls)
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-02.txt")
-    .then((stanza2) => {
-      exerciseUtils.blue(stanza2);
-      return exerciseUtils.promisifiedReadFile("poem-one/stanza-03.txt");
-    })
-    .then((stanza3) => {
-      exerciseUtils.blue(stanza3);
-      console.log("done");
-    });
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-02.txt")
+  //   .then((stanza2) => {
+  //     exerciseUtils.blue(stanza2);
+  //     return exerciseUtils.promisifiedReadFile("poem-one/stanza-03.txt");
+  //   })
+  //   .then((stanza3) => {
+  //     exerciseUtils.blue(stanza3);
+  //     console.log("done");
+  //   });
 }
 
 function problemD() {
   // callback version
-  // exerciseUtils.readFile(
-  //   "poem-one/wrong-file-name.txt",
-  //   function (err, stanza4) {
-  //     if (err) exerciseUtils.magenta(new Error(err));
-  //     else exerciseUtils.blue(stanza4);
-  //   }
-  // );
+  exerciseUtils.readFile(
+    "poem-one/wrong-file-name.txt",
+    function (err, stanza4) {
+      if (err) exerciseUtils.magenta(new Error(err));
+      else exerciseUtils.blue(stanza4);
+    }
+  );
 
   // promise version
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/wrong-file-name.txt")
-    .then((stanza) => exerciseUtils.blue(stanza))
-    .catch((err) => exerciseUtils.magenta(new Error(err)));
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/wrong-file-name.txt")
+  //   .then((stanza) => exerciseUtils.blue(stanza))
+  //   .catch((err) => exerciseUtils.magenta(new Error(err)));
 }
 
 function problemE() {
   // callback version
-  // exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-  //   if (err) return exerciseUtils.magenta(new Error(err));
-  //   exerciseUtils.blue(stanza3);
-  //   exerciseUtils.readFile(
-  //     "poem-one/wrong-file-name.txt",
-  //     function (err2, stanza4) {
-  //       if (err2) return exerciseUtils.magenta(new Error(err2));
-  //       exerciseUtils.blue(stanza4);
-  //     }
-  //   );
-  // });
+  exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+    if (err) return exerciseUtils.magenta(new Error(err));
+    exerciseUtils.blue(stanza3);
+    exerciseUtils.readFile(
+      "poem-one/wrong-file-name.txt",
+      function (err2, stanza4) {
+        if (err2) return exerciseUtils.magenta(new Error(err2));
+        exerciseUtils.blue(stanza4);
+      }
+    );
+  });
 
   // promise version
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-03.txt")
-    .then((stanza3) => {
-      exerciseUtils.blue(stanza3);
-      exerciseUtils
-        .promisifiedReadFile("poem-one/wrong-file-name.txt")
-        .then((stanza4) => {
-          exerciseUtils.blue(stanza4);
-        })
-        .catch((err) => exerciseUtils.magenta(new Error(err)));
-    });
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-03.txt")
+  //   .then((stanza3) => {
+  //     exerciseUtils.blue(stanza3);
+  //     exerciseUtils
+  //       .promisifiedReadFile("poem-one/wrong-file-name.txt")
+  //       .then((stanza4) => {
+  //         exerciseUtils.blue(stanza4);
+  //       })
+  //       .catch((err) => exerciseUtils.magenta(new Error(err)));
+  //   });
 }
 
 function problemF() {
   // callback version
-  // exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-  //   if (err) exerciseUtils.magenta(new Error(err));
-  //   exerciseUtils.blue(stanza3);
-  //   exerciseUtils.readFile(
-  //     "poem-one/wrong-file-name.txt",
-  //     function (err2, stanza4) {
-  //       if (err2) exerciseUtils.magenta(new Error(err2));
-  //       else exerciseUtils.blue(stanza4);
-  //     }
-  //   );
-  // });
+  exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+    if (err) exerciseUtils.magenta(new Error(err));
+    exerciseUtils.blue(stanza3);
+    exerciseUtils.readFile(
+      "poem-one/wrong-file-name.txt",
+      function (err2, stanza4) {
+        if (err2) exerciseUtils.magenta(new Error(err2));
+        else exerciseUtils.blue(stanza4);
+      }
+    );
+  });
 
   // promise version
   // Tu código acá:
-  exerciseUtils
-    .promisifiedReadFile("poem-one/stanza-03.txt")
-    .then((stanza3) => {
-      exerciseUtils.blue(stanza3);
-      return exerciseUtils.promisifiedReadFile("poem-one/wrong-file-name.txt");
-    })
-    .then((stanza4) => {
-      exerciseUtils.blue(stanza4);
-      console.log("done");
-    })
-    .catch((err) => {
-      exerciseUtils.magenta(new Error(err));
-      console.log("done");
-    });
+  // exerciseUtils
+  //   .promisifiedReadFile("poem-one/stanza-03.txt")
+  //   .then((stanza3) => {
+  //     exerciseUtils.blue(stanza3);
+  //     return exerciseUtils.promisifiedReadFile("poem-one/wrong-file-name.txt");
+  //   })
+  //   .then((stanza4) => {
+  //     exerciseUtils.blue(stanza4);
+  //     console.log("done");
+  //   })
+  //   .catch((err) => {
+  //     exerciseUtils.magenta(new Error(err));
+  //     console.log("done");
+  //   });
 }
--- 03-Promises/homework/01 - Exercises/tests/callbacks.test.js
+++ 03-Promises/homework/01 - Exercises/tests/callbacks.test.js
@@ -12,25 +12,35 @@ const {
 let fs = require("fs");
 let path = require("path");
 let utils = require("../utils");
-let dirpath = path.join(__dirname, "../poem-one");
+let dirpathOne = path.join(__dirname, "../poem-one");
+let dirpathTwo = path.join(__dirname, "../poem-two");
 
-let stanzas = fs
-  .readdirSync(dirpath)
+let stanzasOne = fs
+  .readdirSync(dirpathOne)
   .filter((filename) => {
     return filename[0] !== ".";
   })
   .map((filename) => {
-    return fs.readFileSync(path.join(dirpath, filename)).toString();
+    return fs.readFileSync(path.join(dirpathOne, filename)).toString();
   });
 
-describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
+let stanzasTwo = fs
+  .readdirSync(dirpathTwo)
+  .filter((filename) => {
+    return filename[0] !== ".";
+  })
+  .map((filename) => {
+    return fs.readFileSync(path.join(dirpathTwo, filename)).toString();
+  });
+
+xdescribe("01 | Ejercicios - Callbacks (Poem-one)", () => {
   afterAll(async () => {
     await new Promise((resolve) => setTimeout(() => resolve(), 250));
   });
 
   beforeEach(() => {
     jest.spyOn(utils, "readFile").mockImplementation((filename, callback) => {
-      let stanza = stanzas.find((stanza) => stanza.includes(filename));
+      let stanza = stanzasOne.find((stanza) => stanza.includes(filename));
       callback(null, stanza);
     });
   });
@@ -43,8 +53,8 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
     if (readFileSpy.mock.calls.length === 0)
       return done(new Error("problemA | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
-    readFileCallback(null, stanzas[0]);
-    expect(blue).toHaveBeenCalledWith(stanzas[0]);
+    readFileCallback(null, stanzasOne[0]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[0]);
     blue.mockRestore();
     readFileSpy.mockRestore();
     done();
@@ -59,10 +69,10 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
       return done(new Error("problemB | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
     const readFileCallbackTwo = readFileSpy.mock.calls[1][1];
-    readFileCallback(null, stanzas[1]);
-    expect(blue).toHaveBeenCalledWith(stanzas[1]);
-    readFileCallbackTwo(null, stanzas[2]);
-    expect(blue).toHaveBeenCalledWith(stanzas[2]);
+    readFileCallback(null, stanzasOne[1]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[1]);
+    readFileCallbackTwo(null, stanzasOne[2]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[2]);
     blue.mockRestore();
     readFileSpy.mockRestore();
     done();
@@ -77,10 +87,10 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
       return done(new Error("problemC | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
     const readFileCallbackTwo = readFileSpy.mock.calls[1][1];
-    readFileCallback(null, stanzas[1]);
-    expect(blue).toHaveBeenCalledWith(stanzas[1]);
-    readFileCallbackTwo(null, stanzas[2]);
-    expect(blue).toHaveBeenCalledWith(stanzas[2]);
+    readFileCallback(null, stanzasOne[1]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[1]);
+    readFileCallbackTwo(null, stanzasOne[2]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[2]);
     blue.mockRestore();
     readFileSpy.mockRestore();
     done();
@@ -95,8 +105,8 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
     if (readFileSpy.mock.calls.length === 0)
       return done(new Error("problemD | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
-    readFileCallback(null, stanzas[3]);
-    expect(blue).toHaveBeenCalledWith(stanzas[3]);
+    readFileCallback(null, stanzasOne[3]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[3]);
     readFileCallback("Error", null);
     expect(magenta).toHaveBeenCalledWith(new Error("Error"));
     blue.mockRestore();
@@ -115,12 +125,12 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
       return done(new Error("problemE | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
     const readFileCallbackTwo = readFileSpy.mock.calls[1][1];
-    readFileCallback(null, stanzas[2]);
-    expect(blue).toHaveBeenCalledWith(stanzas[2]);
+    readFileCallback(null, stanzasOne[2]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[2]);
     readFileCallback("Error", null);
     expect(magenta).toHaveBeenCalledWith(new Error("Error"));
-    readFileCallbackTwo(null, stanzas[3]);
-    expect(blue).toHaveBeenCalledWith(stanzas[3]);
+    readFileCallbackTwo(null, stanzasOne[3]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[3]);
     readFileCallbackTwo("Error", null);
     expect(magenta).toHaveBeenCalledWith(new Error("Error"));
     blue.mockRestore();
@@ -139,12 +149,12 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
       return done(new Error("problemF | No se llamo a readFile"));
     const readFileCallback = readFileSpy.mock.calls[0][1];
     const readFileCallbackTwo = readFileSpy.mock.calls[1][1];
-    readFileCallback(null, stanzas[2]);
-    expect(blue).toHaveBeenCalledWith(stanzas[2]);
+    readFileCallback(null, stanzasOne[2]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[2]);
     readFileCallback("Error", null);
     expect(magenta).toHaveBeenCalledWith(new Error("Error"));
-    readFileCallbackTwo(null, stanzas[3]);
-    expect(blue).toHaveBeenCalledWith(stanzas[3]);
+    readFileCallbackTwo(null, stanzasOne[3]);
+    expect(blue).toHaveBeenCalledWith(stanzasOne[3]);
     readFileCallbackTwo("Error", null);
     expect(magenta).toHaveBeenCalledWith(new Error("Error"));
     blue.mockRestore();
@@ -152,4 +162,33 @@ describe("01 | Ejercicios - Callbacks (Poem-one)", () => {
     readFileSpy.mockRestore();
     done();
   });
+});
+
+describe("01 | Ejercicios - Callbacks (Poem-two)", () => {
+  afterAll(async () => {
+    await new Promise((resolve) => setTimeout(() => resolve(), 250));
+  });
+
+  beforeEach(() => {
+    jest.spyOn(utils, "readFile").mockImplementation((filename, callback) => {
+      let stanza = stanzasTwo.find((stanza) => stanza.includes(filename));
+      callback(null, stanza);
+    });
+  });
+
+  it("Problem A | Consologuea la primera stanza versión callback", (done) => {
+    jest.setTimeout(500);
+    const blue = jest.spyOn(utils, "blue");
+    const readFileSpy = jest.spyOn(utils, "readFile");
+    problemA();
+    if (readFileSpy.mock.calls.length === 0)
+      return done(new Error("problemA | No se llamo a readFile"));
+    const readFileCallback = readFileSpy.mock.calls[0][1];
+    readFileCallback(null, stanzasTwo[0]);
+    expect(blue).toHaveBeenCalledWith(stanzasTwo[0]);
+    blue.mockRestore();
+    readFileSpy.mockRestore();
+    done();
+  });
+  
 });
