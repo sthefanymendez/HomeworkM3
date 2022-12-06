90a722a9e1d44469c5ccbdde4df8c96f44cc354e
FelipeCiroM <felipe.cirom@udea.edu.co>
2022-12-06 11:23:49 -0500

    clean integration 01-node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,22 +1,8 @@
 const process = require('process');
 const commands = require('./commands/index.js');
 
-function print(output) {
-   process.stdout.write(output);
-   process.stdout.write('\nprompt > ');
-}
-
 function bash() {
-   process.stdout.write('prompt > ');
-   process.stdin.on('data', function (data) {
-      const args = data.toString().trim().split(' ');
-      var cmd = args.shift();
-      if (commands[cmd]) {
-         commands[cmd](print, args.join(' '));
-      } else {
-         print(`command not found: ${cmd}`);
-      }
-   });
+   
 }
 
 bash()
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -3,55 +3,27 @@ const utils = require('../utils/request');
 const process = require('process');
 
 function pwd(print) {
-   print(process.cwd());
 }
 
 function date(print) {
-   print(Date());
 }
 
 function echo(print, args) {
-   print(args);
 }
 
 function ls(print) {
-   fs.readdir('.', function (err, files) {
-      if (err) throw err;
-      print(files.join(' '));
-   });
 }
 
 function cat(print, args) {
-   fs.readFile(args, 'utf-8', (err, data) => {
-      if (err) throw err;
-      print(data);
-   });
 }
 
 function head(print, args) {
-   fs.readFile(args, 'utf-8', (err, data) => {
-      if (err) throw err;
-      print(data.split('\n')[0]);
-   });
 }
 
 function tail(print, args) {
-   fs.readFile(args, 'utf-8', (err, data) => {
-      if (err) {
-         throw err;
-      }
-      const dataArr = data.split('\n');
-      print(dataArr[dataArr.length - 1].trim());
-   });
 }
 
 function curl(print, args) {
-   utils.request(args, function (err, response) {
-      if (err) {
-         throw err;
-      }
-      print(response);
-   });
 }
 
 module.exports = {
