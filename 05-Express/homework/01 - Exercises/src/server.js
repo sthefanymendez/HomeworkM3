c58de7751d776c2983fe66f8e2c0a38d911b4842
Martín Nicolás Precelle <tinchoprecelle@gmail.com>
2022-12-12 20:36:50 -0300

    Resolving post exercise

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -9,9 +9,22 @@ const posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
+
+let id = 0;
+
+server.post("/posts", (req, res) => {
+  const { author, title, contents } = req.body;
+  if (!author || !title || !contents)
+    res.status(STATUS_USER_ERROR).json({
+      error: "No se recibieron los parámetros necesarios para crear el Post",
+    });
+
+  const post = { id: ++id, author, title, contents };
+  posts.push(post);
+  res.json(post);
+});
 
 // TODO: your code to handle requests
 
-
-module.exports = { posts, server };
+module.exports = server;
