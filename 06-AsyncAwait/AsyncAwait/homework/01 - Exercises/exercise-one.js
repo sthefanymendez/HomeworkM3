664dc530e8e1f18c6f93d886ab724cc8b325473d
Daiana <daiana_agustina@outlook.com>
2022-12-15 14:37:32 -0300

    testing HW 06

--- 06-AsyncAwait/AsyncAwait/homework/01 - Exercises/exercise-one.js
+++ 06-AsyncAwait/AsyncAwait/homework/01 - Exercises/exercise-one.js
@@ -11,15 +11,9 @@
 
 "use strict";
 
-var Promise = require("bluebird"),
-  exerciseUtils = require("./utils");
+let exerciseUtils = require("./utils");
 
-var readFile = exerciseUtils.readFile,
-  promisifiedReadFile = exerciseUtils.promisifiedReadFile,
-  blue = exerciseUtils.blue,
-  magenta = exerciseUtils.magenta;
-
-var args = process.argv.slice(2).map(function (st) {
+let args = process.argv.slice(2).map(function (st) {
   return st.toUpperCase();
 });
 
@@ -38,86 +32,104 @@ args.forEach(function (arg) {
   if (problem) problem();
 });
 
-function problemA() {
+async function problemA() {
   // callback version
-  readFile("poem-one/stanza-01.txt", function (err, stanza) {
-    console.log("-- A. callback version --");
-    blue(stanza);
-  });
+  // exerciseUtils.readFile("poem-one/stanza-01.txt", function (err, stanza) {
+  //   console.log("-- A. callback version --");
+  //   exerciseUtils.blue(stanza);
+  // });
 
   // AsyncAwait version
+  const stanza = await exerciseUtils.promisifiedReadFile('poem-one/stanza-01.txt')
+  exerciseUtils.blue(stanza)
 }
 
-function problemB() {
+async function problemB() {
   // callback version
-  readFile("poem-one/stanza-02.txt", function (err, stanza2) {
-    console.log("-- B. callback version (stanza two) --");
-    blue(stanza2);
-  });
-  readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-    console.log("-- B. callback version (stanza three) --");
-    blue(stanza3);
-  });
+  // exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
+  //   console.log("-- B. callback version (stanza two) --");
+  //   exerciseUtils.blue(stanza2);
+  // });
+  // exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+  //   console.log("-- B. callback version (stanza three) --");
+  //   exerciseUtils.blue(stanza3);
+  // });
 
   // AsyncAwait version
+  const stanza2 = await exerciseUtils.promisifiedReadFile('poem-one/stanza-02.txt')
+  const stanza3 = await exerciseUtils.promisifiedReadFile('poem-one/stanza-03.txt')
+  exerciseUtils.blue(stanza2)
+  exerciseUtils.blue(stanza3)
 }
 
-function problemC() {
+async function problemC() {
   // callback version
-  readFile("poem-one/stanza-02.txt", function (err, stanza2) {
-    console.log("-- C. callback version (stanza two) --");
-    blue(stanza2);
-    readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-      console.log("-- C. callback version (stanza three) --");
-      blue(stanza3);
-      console.log("-- C. callback version done --");
-    });
-  });
+  // exerciseUtils.readFile("poem-one/stanza-02.txt", function (err, stanza2) {
+  //   console.log("-- C. callback version (stanza two) --");
+  //   exerciseUtils.blue(stanza2);
+  //   exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+  //     console.log("-- C. callback version (stanza three) --");
+  //     exerciseUtils.blue(stanza3);
+  //     console.log("-- C. callback version done --");
+  //   });
+  // });
 
   // AsyncAwait version
+  const stanza2 = await exerciseUtils.promisifiedReadFile('poem-one/stanza-02.txt')
+  const stanza3 = await exerciseUtils.promisifiedReadFile('poem-one/stanza-03.txt')
+  exerciseUtils.blue(stanza2)
+  exerciseUtils.blue(stanza3)
+  console.log('done');
 }
 
-function problemD() {
+async function problemD() {
   // callback version
-  readFile("poem-one/wrong-file-name.txt", function (err, stanza4) {
-    console.log("-- D. callback version (stanza four) --");
-    if (err) magenta(err);
-    else blue(stanza4);
-  });
+  // exerciseUtils.readFile("poem-one/wrong-file-name.txt", function (err, stanza4) {
+  //   console.log("-- D. callback version (stanza four) --");
+  //   if (err) exerciseUtils.magenta(new Error(err));
+  //   else exerciseUtils.blue(stanza4);
+  // });
 
   // AsyncAwait version
+  try {
+    const stanza = await exerciseUtils.promisifiedReadFile('poem-one/wrong-file-name.txt')
+    exerciseUtils.blue(stanza)
+  } catch (error) {
+    console.log('Problem: ', error);
+    exerciseUtils.magenta(error)
+  }
 }
 
 function problemE() {
   // callback version
-  readFile("poem-one/stanza-03.txt", function (err, stanza3) {
-    console.log("-- E. callback version (stanza three) --");
-    if (err) return magenta(err);
-    blue(stanza3);
-    readFile("poem-one/wrong-file-name.txt", function (err2, stanza4) {
-      console.log("-- E. callback version (stanza four) --");
-      if (err2) return magenta(err2);
-      blue(stanza4);
-    });
-  });
+  // exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+  //   console.log("-- E. callback version (stanza three) --");
+  //   if (err) return exerciseUtils.magenta(new Error(err));
+  //   exerciseUtils.blue(stanza3);
+  //   exerciseUtils.readFile("poem-one/wrong-file-name.txt", function (err2, stanza4) {
+  //     console.log("-- E. callback version (stanza four) --");
+  //     if (err2) return exerciseUtils.magenta(err2);
+  //     exerciseUtils.blue(stanza4);
+  //   });
+  // });
 
   // AsyncAwait version
 }
 
 function problemF() {
   // callback version
-  readFile("poem-one/stanza-03.txt", function (err, stanza3) {
+  exerciseUtils.readFile("poem-one/stanza-03.txt", function (err, stanza3) {
     console.log("-- F. callback version (stanza three) --");
     if (err) {
-      magenta(err);
+      if (err) exerciseUtils.magenta(new Error(err));
       console.log("-- F. callback version done --");
       return;
     }
-    blue(stanza3);
-    readFile("poem-one/wrong-file-name.txt", function (err2, stanza4) {
+    exerciseUtils.blue(stanza3);
+    exerciseUtils.readFile("poem-one/wrong-file-name.txt", function (err2, stanza4) {
       console.log("-- F. callback version (stanza four) --");
-      if (err2) magenta(err2);
-      else blue(stanza4);
+      if (err2) exerciseUtils.magenta(new Error(err2));
+      else exerciseUtils.blue(stanza4);
       console.log("-- F. callback version done --");
     });
   });
