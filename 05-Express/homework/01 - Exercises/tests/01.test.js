01997794a550bdee8a851824965c645fb4168773
MPrecelle <tinchoprecelle@gmail.com>
2023-01-18 14:44:53 -0300

    Deleting testing 2A, and adding errors in 2

--- 05-Express/homework/01 - Exercises/tests/01.test.js
+++ 05-Express/homework/01 - Exercises/tests/01.test.js
@@ -32,51 +32,10 @@ describe("01 | Ejercicios", () => {
     expect(publications).toContainEqual(newPost, newPost2);
   });
 
-  it("2A | GET a la ruta /posts, si existe la query 'term' devuelve los posts que coincidan con ese query. En caso de que no exista, devuelve todos los posts", async () => {
-    const response = await api.get("/posts");
-    expect(response.status).toBeGreaterThanOrEqual(200);
-    expect(response.status).toBeLessThan(300);
-    expect(response.body).toEqual(publications);
-    const addingPost = await api.post("/posts").send({
-      id: ++id,
-      author: "Author Test 3",
-      title: "Title Test 3",
-      contents: "Content Testing 3",
-    });
-    expect(addingPost.status).toBeGreaterThanOrEqual(200);
-    expect(addingPost.status).toBeLessThan(300);
-    const response2 = await api.get("/posts");
-    expect(response2.status).toBeGreaterThanOrEqual(200);
-    expect(response2.status).toBeLessThan(300);
-    expect(response2.body).toEqual(publications);
-    const response3 = await api.get("/posts").query({ term: "Testing" });
-    expect(response3.status).toBeGreaterThanOrEqual(200);
-    expect(response3.status).toBeLessThan(300);
-    const termQuery = response3.req.path.split("?")[1].split("=")[1];
-    const filteredPosts = publications.filter(
-      (post) =>
-        post.title.includes(termQuery) || post.contents.includes(termQuery)
-    );
-    filteredPosts.length > 0
-      ? expect(response3.body).toEqual(filteredPosts)
-      : expect(response3.body).toEqual(publications);
-    const response4 = await api.get("/posts").query({ term: "Not Found" });
-    expect(response4.status).toBeGreaterThanOrEqual(200);
-    expect(response4.status).toBeLessThan(300);
-    const termQuery2 = response4.req.path.split("?")[1].split("=")[1];
-    const filteredPosts2 = publications.filter(
-      (post) =>
-        post.title.includes(termQuery2) || post.contents.includes(termQuery2)
-    );
-    filteredPosts2.length > 0
-      ? expect(response4.body).toEqual(filteredPosts2)
-      : expect(response4.body).toEqual(publications);
-  });
-
-  it("2B | GET a la ruta /posts, si existen las querys 'author' y 'title', devuelve los posts que coincidan con ambas querys. En caso de que no exista, devuelve un error", async () => {
+  it("2 | GET a la ruta /posts, si existen las querys 'author' y 'title', devuelve los posts que coincidan con ambas querys. En caso de que no exista, devuelve un error", async () => {
     const response = await api
       .get("/posts")
-      .query({ author: "Author Test", title: "Title Tesasdt" });
+      .query({ author: "Author Test", title: "Title Test" });
     if (response.body.length > 0) {
       expect(response.status).toBeGreaterThanOrEqual(200);
       expect(response.status).toBeLessThan(300);
@@ -98,6 +57,34 @@ describe("01 | Ejercicios", () => {
       expect(response.status).toBeGreaterThanOrEqual(400);
       expect(response.status).toBeLessThan(500);
       expect(response.body).toEqual({
+        error:
+          "No existe ninguna publicación con dicho título y autor indicado",
+      });
+    }
+    const response2 = await api
+      .get("/posts")
+      .query({ author: "Author Test 2", title: "Title Test Henry" });
+    if (response2.body.length > 0) {
+      expect(response2.status).toBeGreaterThanOrEqual(200);
+      expect(response2.status).toBeLessThan(300);
+      const authorQuery2 = response2.req.path
+        .split("?")[1]
+        .split("&")[0]
+        .split("=")[1]
+        .replace("%20", " ");
+      const titleQuery2 = response2.req.path
+        .split("?")[1]
+        .split("&")[1]
+        .split("=")[1]
+        .replace("%20", " ");
+      const filteredPosts2 = publications.filter(
+        (post) => post.author === authorQuery2 && post.title === titleQuery2
+      );
+      expect(response2.body).toEqual(filteredPosts2);
+    } else {
+      expect(response2.status).toBeGreaterThanOrEqual(400);
+      expect(response2.status).toBeLessThan(500);
+      expect(response2.body).toEqual({
         error:
           "No existe ninguna publicación con dicho título y autor indicado",
       });
