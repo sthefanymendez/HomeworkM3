c31325c75d18b6ce4d3b197f5c222d126dc6653c
Daiana <daiana_agustina@outlook.com>
2023-02-14 17:04:48 -0300

    [EDIT] test HW 06

--- 06-AsyncAwait/homework/01 - Exercises/tests/asyncawait.test.js
+++ 06-AsyncAwait/homework/01 - Exercises/tests/asyncawait.test.js
@@ -202,7 +202,7 @@ describe("02 | Ejercicios - Async Await (poem-two)", () => {
     expect(promisifiedReadFileSpy).toHaveBeenCalled(); // problemC | No se llamó a promisifiedReadFile
   });
 
-  it.only("Problem D | Consologuea todas las stanzas de poem-two en orden o un error, versión Async Await", async () => {
+  it("Problem D | Consologuea todas las stanzas de poem-two en orden o un error, versión Async Await", async () => {
     const blue = jest.spyOn(utils, "blue");
     const magenta = jest.spyOn(utils, "magenta");
     const promisifiedReadFileSpy = jest.spyOn(utils, "promisifiedReadFile");
